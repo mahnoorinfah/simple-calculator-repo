d1b8c3b2f40a05b234150c64503664b7b6c95793
MahnoorShafiq <161619478+mahnoorinfah@users.noreply.github.com>
2024-06-04 07:14:19 -0700

    updated

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 "use strict";
 let a = "Hello";
 console.log(a);
+let num1 = 4;
+let num2 = 6;
+let add = num1 + num2;
+console.log("result of addition", add);
--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 let a = "Hello";
 console.log(a);
+let num1: number = 4;
+let num2: number = 6;
+let add: number = num1 + num2;
+console.log("result of addition", add);
+
